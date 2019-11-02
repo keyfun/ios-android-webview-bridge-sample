e6fefe194eb8a296888ed68e9f15b46adde7a8bb
Key Hui <>
2019-11-02 21:40:09 +0800

    complete iOS demo

--- WebApp/app/scripts/main.js
+++ WebApp/app/scripts/main.js
@@ -1,2 +1,25 @@
-console.log('\'Allo \'Allo!');
+function sendDataToApp() {
+    var response = {
+        data: {
+            name: "keyName",
+            stringValue: "myValue",
+            numberValue: 123
+        }
+    }
+    if (window.DemoWebInterface) {
+        // Call Android interface
+        window.DemoWebInterface.postMessage(JSON.stringify(response))
+      } else if (
+        window.webkit &&
+        window.webkit.messageHandlers &&
+        window.webkit.messageHandlers.demo
+      ) {
+        // Call iOS interface
+        window.webkit.messageHandlers.demo.postMessage(response)
+      } else {
+        // No Android or iOS interface found
+        console.log('No native APIs found.')
+        alert('No native APIs found.')
+      }
+}
 
--- iOSApp/iOSApp/ViewController.swift
+++ iOSApp/iOSApp/ViewController.swift
@@ -7,14 +7,98 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, WKScriptMessageHandler {
 
+    var webView: WKWebView!
+    let host = "localhost"
+    let kHandlerName = "demo"
+    let urlPath = "http://localhost:9000"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        initWebView()
+        loadUrl(urlPath)
+    }
+    
+    func initWebView() {
+        let webViewFrame = getWebViewFrame()
+        webView = WKWebView(frame: webViewFrame, configuration: getConfiguration())
+        view.addSubview(webView)
+
+        webView.navigationDelegate = self
+        webView.uiDelegate = self
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.title), options: .new, context: nil)
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if let host = navigationAction.request.url?.host {
+            if host == self.host {
+                decisionHandler(.allow)
+                return
+            }
+        }
+        print("cancel: \(String(describing: navigationAction.request.url?.host))")
+        decisionHandler(.cancel)
     }
 
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        let ac = UIAlertController(title: "iOS Alert", message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(ac, animated: true)
+        completionHandler()
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("didFinish")
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            print(Float(webView.estimatedProgress))
+        }
+
+        if keyPath == "title" {
+            if let title = webView.title {
+                print(title)
+            }
+        }
+    }
+
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        guard let body = message.body as? [String: Any] else { return }
+        guard let data = body["data"] as? [String: Any] else { return }
+        print(data)
+        
+        let ac = UIAlertController(title: "Message Received:", message: data.description, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(ac, animated: true)
+    }
+
+    func getWebViewFrame() -> CGRect {
+        return CGRect(x: 0.0, y: 0.0, width: view.frame.width, height: view.frame.height)
+    }
+
+    func loadUrl(_ urlPath: String) {
+        if let url = URL(string: urlPath) {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
+    
+    private func getConfiguration() -> WKWebViewConfiguration {
+        let configuration = WKWebViewConfiguration()
+
+        let userContentController = WKUserContentController()
+        userContentController.add(self, name: kHandlerName)
+
+        configuration.dataDetectorTypes = [.all]
+        configuration.userContentController = userContentController
+
+        return configuration
+    }
 
 }
 
